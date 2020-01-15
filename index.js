521a73ffed936699c13c8fe531e1acb53e8561ad
Warren Yao <warrenyca905@gmail.com>
2020-01-15 01:01:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
