c57610ab0b65d35b6fdc6fa976591751737147d7
Warren Yao <warrenyca905@gmail.com>
2020-01-15 01:03:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight() {
   let rightNumbers = dodger.style.right.replace("px", "");
   let right = parseInt(rightNumbers, 10);
  
-  if (right < 180) {
+  if (right <= 180) {
     dodger.style.right = `${right + 1}px`;
   }
 }
