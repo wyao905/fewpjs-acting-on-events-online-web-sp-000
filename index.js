aa9e0e7e7bb130dd16b7056c09c37d7fdd7c43cb
Warren Yao <warrenyca905@gmail.com>
2020-01-15 01:02:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,17 +10,23 @@ function moveDodgerLeft() {
   }
 }
 
-function moveDodgerLeft() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
  
-  if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+  if (right < 180) {
+    dodger.style.right = `${right + 1}px`;
   }
 }
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
+  }
+});
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
   }
 });
